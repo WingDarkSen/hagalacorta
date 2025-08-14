061b2643011fcf859d1a8df39678bbf772e9168b
WingDarkSen <wdks@wleonDEGCCSS.localdomain>
2025-08-14 15:57:41 -0400

    Agrego endpoint /ping para cronjob

--- app.py
+++ app.py
@@ -54,6 +54,12 @@ def estadisticas(codigo):
 #    app.run(debug=True)
 #
 
+@app.route('/ping')
+def ping():
+    return 'pong', 200
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
